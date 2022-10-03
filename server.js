fb5803db60ec959fb5c9f88663e968f666032521
krogo81611 <krogo81611@gmail.com>
2022-10-03 10:02:59 -0600

    added routes files and populated public folder

--- server.js
+++ server.js
@@ -7,4 +7,23 @@ const MongoStore = require("connect-mongo")(session);
 const methodOverride = require("method-override");
 const flash = require("express-flash");
 const logger = require("morgan");
-const connectDB = require("./config/database")
+const connectDB = require("./config/database");
+//Tell server where to access the routes for each aspect of application
+const mainRoutes = require("./routes/main");
+const postRoutes = require("./routes/posts");
+const commentRoutes = require("./routes/comments");
+
+//Use .env file in config folder
+require("dotenv").config({ path: "./config/.env"});
+
+//Passport config
+require("./config/passport")(passport);
+
+//Connect to Database
+connectDB();
+
+//Using EJS for views
+app.set("view engine", "ejs");
+
+//Static Folder tells Server where to find css/js/assets
+app.use(express.static("public"));
